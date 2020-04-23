e2b2f44140b1a05c28b8993fe9735d032eee6a85
Cooklord <imnoahcook@gmail.com>
2020-04-23 00:24:01 -0400

    move server code into webserver file

--- server/src/index.js
+++ server/src/index.js
@@ -1,20 +1,3 @@
-const express = require('express');
+const webserver = require('./webserver');
 
-const app = express();
-const port = 3000;
-
-// http GET request
-app.get('*', (req, res) => res.json({ message: 'no endpoint' }));
-
-// make this return the param yared * 10
-app.get('/sciencebowl/:yared', (req, res) => {
-  res.send(req.params);
-});
-
-app.get('/usapho/:thermodynamics', (req, res) => {
-  res.send(req.params);
-});
-
-app.listen(port, () =>
-  console.log(`🔥 app listening at http://localhost:${port}`)
-);
+// everything in the import happens before 
